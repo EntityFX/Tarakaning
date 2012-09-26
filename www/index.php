dd6e4df296127d5c7ea02d48190bd481d584174f
EntityFX <EntityFX@f7bc01b6-7233-4688-9e6f-3fd5e8432e91>
2012-09-26 09:57:09 +0000

    * Создана файловая структура приложения yii * Добавлен компонент рендерера Smarty yii * Добавлена библиотека Smarty

--- www/index.php
+++ www/index.php
@@ -1,25 +1,13 @@
 <?php
-    
-    /**
-    * @global Const Ïóòü ê ôàéëàì
-    * @name SOURCE_PATH
-    */
-    define("SOURCE_PATH",dirname(__FILE__).'/');
-    
-    /**
-    * Ïîäêëş÷àåò êîíñòàíòû ÁÄ
-    * @filesource config/databaseConsts.php 
-    */    
-    require_once SOURCE_PATH."engine/config/globals.php";    
-    
-    require_once SOURCE_PATH."engine/kernel/FrontController.php";
-    
-    /**
-    * Âàæíûé ıêçåìïëÿğ êëàññà
-    * 
-    * @var Kernel
-    */
-    $kernel=FrontController::getInstance();
-    //FrontController::setGlobalEncoding("UTF-8");
-    $kernel->run();    
-?>
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
