cb32dfd373c80053446057559f609231f02cf72e
EntityFX <EntityFX@f7bc01b6-7233-4688-9e6f-3fd5e8432e91>
2012-10-02 23:30:58 +0000

    * Added Twig templator * View refactoring * Services refactoring * Added captcha

---
+++ branches/yii/www/protected/components/services/Projects/MyProjectsFieldsENUM.php
@@ -0,0 +1,19 @@
+<?php
+
+Loader::LoadSystem('AEnum'); 
+    
+final class MyProjectsFieldsENUM extends AEnum
+{
+    const PROJECT_NAME  	= "ProjectName";
+    const DESCRIPTION      	= "Description";
+    const COUNT_USERS      	= "CountUsers";
+    const COUNT_REQUESTS    = "CountSubscribeRequests";
+    const CREATE_DATE      	= "CreateDateTime";
+    const NICK_NAME      	= "OwnerNickName";
+                                    
+    public function __construct($value=self::PROJECT_NAME)
+    {
+        parent::__construct($value);
+    }
+} 
+?>
